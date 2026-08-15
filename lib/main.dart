ffede86b1c2c373998b681010a6df5376df4c697
Abbas_Khan <abbaskhanshah10@gmail.com>
2026-08-15 11:54:50 +0500

    today i learn about stateLess widget and also about colors and how to add fonts to flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
+  home: HomePage(),
+));
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
     appBar: AppBar(
       title: Text("Hello gang!"),
+      centerTitle: true,
+      backgroundColor: Colors.deepOrangeAccent,
     ),
-  ),
-));
+    body: Center(
+      child: Text(
+        "Hello Ninjas",
+        style: TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () => {},
+      backgroundColor: Colors.deepOrange,
+      foregroundColor: Colors.black,
+      child: Text("Click"),
+    ),
+  );
+  }
+}
