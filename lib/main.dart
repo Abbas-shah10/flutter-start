5eb5870e095e4027f3544e17be7121326f2caff8
Abbas_Khan <abbaskhanshah10@gmail.com>
2026-08-15 12:46:40 +0500

    Learned about Container in flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: HomePage(),
-));
+void main() => runApp(MaterialApp(home: HomePage()));
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-    appBar: AppBar(
-      title: Text("Hello gang!"),
-      centerTitle: true,
-      backgroundColor: Colors.deepOrangeAccent,
-    ),
-    body: Center(
-      child: Text("Hello!"),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () => {},
-      backgroundColor: Colors.deepOrange,
-      foregroundColor: Colors.black,
-      child: Text("Click"),
-    ),
-  );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello gang!"),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Container(
+        color: Colors.grey[400],
+        child: Text("hello"),
+      ),
+    );
   }
 }
