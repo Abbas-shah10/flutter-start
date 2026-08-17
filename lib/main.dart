772a3f0cb294c6ffb31857cd2aebf2d37651effd
Abbas_Khan <abbaskhanshah10@gmail.com>
2026-08-17 07:03:24 +0500

    build the text showing and hiding functionality.....

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: HomePage()));
+void main() => runApp(const MaterialApp(home: HomePage()));
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isVisible = false;
+
+  void toggleVisibility() {
+    setState(() {
+      isVisible = !isVisible;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello gang!"),
+        title: const Text("Hello gang!"),
         centerTitle: true,
         backgroundColor: Colors.deepOrangeAccent,
       ),
       body: Container(
         color: Colors.grey[400],
-        child: Text("hello"),
+        width: double.infinity,
+        child: Center(
+          child: isVisible
+              ? const Text(
+                  "Hello, I am visible now!",
+                  style: TextStyle(fontSize: 24),
+                )
+              : const Text("Click The Button to see Magic"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrangeAccent,
+        onPressed: toggleVisibility,
+        child: const Text("Click"),
       ),
     );
   }
