27bf32ec704a1519a9fbbe50365f19be165ec1df
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-08-23 19:46:37 +0500

    learn some additional things

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,7 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,32 +20,20 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Container(
-            margin: EdgeInsets.only(bottom: 14.0),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 5),
-                  child: Text("username", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight(500)),),
-                ),
-              ],
-            ),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.blue,
+            child: Text("Text1"),
           ),
           Container(
-            margin: const EdgeInsets.only(bottom: 16.0),
-            child: TextField()
-          ),
-          Row(
-            children: [
-              Text("Email", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight(500)),),
-            ],
+            padding: EdgeInsets.all(30.0),
+            color: Colors.red[300],
+            child: Text("Text2"),
           ),
           Container(
-            margin: EdgeInsets.only(bottom: 16.0),
-            child: TextField()
+            padding: EdgeInsets.all(40.0),
+            color: Colors.green,
+            child: Text("Text3"),
           ),
-          SizedBox(
-            child: ElevatedButton(onPressed: () {}, child: Text("Submit"))
-          )
         ],
       ),
     );
